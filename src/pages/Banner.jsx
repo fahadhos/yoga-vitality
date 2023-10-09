e56b641289078669260dc11582ea227630dc4e47
fahadhos <generalus159@gmail.com>
2023-10-09 07:42:27 +0600

    Fixed responsive

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -5,12 +5,12 @@ const Banner = () => {
         <div  data-aos="fade-up"
         data-aos-duration="3000">
 
- <div className="hero min-h-screen  " style={{backgroundImage: 'url(https://i.ibb.co/mbKr6TV/background-1.jpg)'}}
+ <div className="hero min-h-screen max-sm:w-[425px] " style={{backgroundImage: 'url(https://i.ibb.co/mbKr6TV/background-1.jpg)'}}
  
  >
     <div className="hero-overlay bg-opacity-80 bg-slate-600"></div>
-  <div className="hero-content relative text-left -left-[20rem] items-start text-neutral-content">
-    <div className="max-w-md   ">
+  <div className="hero-content relative text-left -left-[16rem] items-start text-neutral-content max-sm:w-[425px]">
+    <div className="max-w-md  max-sm:pl-[20rem]  ">
       <h1 className="mb-5 text-xl border-2 w-[18rem] px-5  relative uppercase font-bold">Yoga Fitness club</h1>
       <p className="mb-5 text-3xl"> 
     <span>Elevate Your  </span>   <br />
