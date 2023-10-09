57882f84bb872a1aa59e8023c238aa47a74e7396
fahadhos <generalus159@gmail.com>
2023-10-09 07:42:47 +0600

    Fixed responsive

--- src/pages/ClassCard.jsx
+++ src/pages/ClassCard.jsx
@@ -8,27 +8,26 @@ const ClassCard = ({item}) => {
 
     return (
         <div>
-      <div className="card w-[20rem] hover:w-[22rem] bg-base-100 shadow-xl">
+      <div className="card w-[20rem] hover:shadow-2xl  hover:w-[20rem] bg-base-100 shadow-xl">
   <figure><img src={item.imageURL} alt={item.name}/></figure>
   <div className="card-body">
-    <h2 className="card-title">
+    <h2 className="card-title bg-gradient-to-r text-transparent bg-clip-text from-cyan-500 to-blue-500">
      {item.name}
          </h2>
    
     
-    {item.description.length>50? <p className="text-left font-sm"> 
+    {item.description.length>50? <p className="text-left font-sm bg-gradient-to-r text-transparent bg-clip-text from-cyan-500 to-blue-500"> 
       {item.description.slice(0,50)}... 
                <br /> 
-           <Link to={`/classes/${item.id}`} className='text-teal-400'>Read More</Link>
-                </p>: <p>{item.description}</p>
+           <Link to={`/classes/${item.id}`} className='bg-gradient-to-r text-transparent bg-clip-text from-cyan-500 to-red-500 font-bold hover:text-yellow-700 hover:text-xl'>Read More</Link>
+                </p>: <p className=" text-left font-sm bg-gradient-to-r text-transparent bg-clip-text from-cyan-500 to-blue-500">{item.description}</p>
            
            }
              
     
     
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div> 
-      <div className="badge badge-outline">Products</div>
+    <div className="card-actions justify-center pt-3">
+      <div className="badge badge-outline border-2 py-3 text-teal-600   border-teal-400">{item.time}</div>  
     </div>
   </div>
 </div>
